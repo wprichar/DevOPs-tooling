f40fe42973e8b780cf9822a3ca152cdb1925705e
Wesley Richards <wprichar@us.ibm.com>
2015-11-17 21:44:43 +0000

    addition of mocha test

--- test/unit/mocha.js
+++ test/unit/mocha.js
@@ -1,3 +1,18 @@
-$(document).ready(function() {
-	printSomething("kajshdsa");
-}
+var chai = require("chai"),
+    expect = chai.expect;
+var mosca = require('mosca');
+
+
+describe( "Mosca Server", function(done){
+
+    describe( "create mosca server", function(){
+        it( "server should start", function(){
+            var output;
+            mqttServe.on('clientConnected', function(client) {
+    			console.log('client connected', client.id);
+    			expect(1).to.equal(1);
+    			done();
+			});
+         });
+    });
+});
