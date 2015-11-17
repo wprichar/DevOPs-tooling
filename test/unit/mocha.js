28c1a97046d530429f55751b527808ffc7440633
Wesley Richards <wprichar@us.ibm.com>
2015-11-17 22:43:58 +0000

    new changes

--- test/unit/mocha.js
+++ test/unit/mocha.js
@@ -6,12 +6,13 @@ var mosca = require('mosca');
 describe( "Mosca Server", function(done){
 
     describe( "create mosca server", function(){
-        it( "server should start", function(){
+        it( "server should start", function(done){
             var output;
             var mqttServe = new mosca.Server({});
             mqttServe.on('clientConnected', function(client) {
     			console.log('client connected', client.id);
     			expect(client.id).to.equal(5);
+    			done();
 			});
          });
     });
