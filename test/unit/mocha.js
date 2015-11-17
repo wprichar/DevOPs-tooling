71745fb0c90745c1ee055a2c9550c3fdfa01504f
Wesley Richards <wprichar@us.ibm.com>
2015-11-17 23:06:31 +0000

    changes wes2

--- test/unit/mocha.js
+++ test/unit/mocha.js
@@ -9,7 +9,7 @@ describe( "Mosca Server", function(done){
         it( "server should start", function(done){
             var output;
             var mqttServe = new mosca.Server({});
-            mqTTServe.on('ready', setup);
+            mqttServe.on('ready', setup);
             function setup() {
   					console.log('Mosca server is up and running');
   					expect(client.id).to.equal(5);
