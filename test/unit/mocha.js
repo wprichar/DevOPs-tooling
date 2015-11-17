92be710a4d1c3e42359751acbf8bc7fec798e792
Wesley Richards <wprichar@us.ibm.com>
2015-11-17 21:50:00 +0000

    addition of server

--- test/unit/mocha.js
+++ test/unit/mocha.js
@@ -8,6 +8,7 @@ describe( "Mosca Server", function(done){
     describe( "create mosca server", function(){
         it( "server should start", function(){
             var output;
+            var mqttServe = new mosca.Server({});
             mqttServe.on('clientConnected', function(client) {
     			console.log('client connected', client.id);
     			expect(1).to.equal(1);
