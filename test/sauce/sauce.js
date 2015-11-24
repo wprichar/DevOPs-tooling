e4dffefaeae24458659c941f7dd1b64f42d094a6
Wesley Richards <wprichar@us.ibm.com>
2015-11-24 20:30:18 +0000

    asdsadasdadsasdasdsad

--- test/sauce/sauce.js
+++ test/sauce/sauce.js
@@ -89,7 +89,7 @@ describe('tutorial (' + desired.browserName + ')', function() {
       .type("test")
       .elementById("send")
       .click()
-      .elementById('messageCount').text
+      .elementById('messageCount').text()
       .should.eventually.equal(1)
       .nodeify(done);
   });
