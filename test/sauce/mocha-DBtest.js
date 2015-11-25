b44011464fc0eb05ec9b80717d0587978279722f
Wesley Richards <wprichar@us.ibm.com>
2015-11-25 22:45:33 +0000

    safasfasfasfas

--- test/sauce/mocha-DBtest.js
+++ test/sauce/mocha-DBtest.js
@@ -13,7 +13,8 @@ describe( "Query database to confirm message was sent ", function(){
 
         it( "send request ... if id property exists message was sent to database", function(done){
               this.timeout(10000);
-              request.get(process.env.url, function (err, res, body){
+              output=process.env.url+'/my_sample_db/_all_docs'
+              request.get( output , function (err, res, body){
                      expect(res.statusCode).to.equal(200);
                      output = JSON.parse(res.body);
                      expect(output.rows[0].id).to.not.equal(null);
